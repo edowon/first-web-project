6b9bf9e6e204ac6ff91e296388699e953e3202bd
Dowon Lee <edowon0623@gmail.com>
2020-03-27 00:14:04 +0900

    first commit for first my web project course

---
+++ src/main/java/org/example/HelloServlet2.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/HelloServlet2")
+public class HelloServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("Good morning.");
+        out.close();
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zitan
+  Date: 2020-03-18
+  Time: 오후 6:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Hello, Kenneth. </h1>
+
+</body>
+</html>
